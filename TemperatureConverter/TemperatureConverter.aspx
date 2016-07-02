e6264de01a94085213a04ef147aaa9443ca694e3
Kavitha Nambiar <kavithamohan.nambiar@gmail.com>
2016-07-02 19:50:18 -0400

    Add files via upload

---
+++ TemperatureConverter/TemperatureConverter.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TemperatureConverter.aspx.cs" Inherits="TemperatureConverter" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Temperature Converter</title>
+    <link rel="stylesheet" type="text/css" href="TemperatureConverter.css">
+	<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+    </head>
+<body>
+    <h1> Temperature Converter </h1>
+    <form runat = "server">
+    
+        <div>
+        Convert: &nbsp; <input type = "text" ID = "Fahr" runat = "server" /> Fahrenheit to &nbsp;
+        <select ID = "Temperature" runat = "server" />
+        <br/> <br/>
+        <input type = "submit" value = "OK" ID = "ConvertOnServerClick" OnServerClick= "Convert_ServerClick" runat = "server" />
+        <br/> <br/>
+        <p style = "font-weight: bold" ID = "Result" runat = "server" > </p>
+        </div>
+
+    </form>
+
+</body>
+</html>
---
+++ TemperatureConverter/TemperatureConverter.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class TemperatureConverter : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        if (this.IsPostBack == false)
+        {
+            // The HtmlSelect control accepts text or ListItem objects.
+            Temperature.Items.Add(new ListItem("Celsius"));
+            Temperature.Items.Add(new ListItem("Kelvin"));
+            
+        }
+    }
+
+    protected void Convert_ServerClick(object sender, EventArgs e)
+    {
+        decimal oldTemp;
+        
+        bool success = Decimal.TryParse(Fahr.Value, out oldTemp);
+        if (success)
+        {
+            // Retrieve the selected ListItem object by its index number.
+            
+            ListItem item = Temperature.Items[Temperature.SelectedIndex];
+            decimal newTemp= 0M;
+
+            switch (Temperature.SelectedIndex)
+            {
+                case 0:
+                    newTemp = ((oldTemp - 32) / (9 / 5));
+                    break;
+                case 1:
+                    newTemp = ((oldTemp - 32) * (5 / 9)) + 273.15M;
+                    break;
+                default:
+                    break;
+            }
+
+            
+
+            Result.InnerText = oldTemp.ToString() + " Fahrenheit = ";
+            Result.InnerText += newTemp.ToString() + " " + item.Text;
+
+          
+        }
+    }
+
+}
